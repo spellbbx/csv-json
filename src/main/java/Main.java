b2ee09b256f4a88f3db41003c09a6654ff8f4331
spellbbx <89066808872@mail.ru>
2023-05-29 23:13:12 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,65 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+        writeString(json);
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        List<Employee> employees = null;
+
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+
+            CsvToBean<Employee> csvToBean = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+
+            employees = csvToBean.parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return employees;
+    }
+
+    private static String listToJson(List<Employee> list) {
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        return gson.toJson(list, listType);
+    }
+
+    private static void writeString(String json) {
+
+        try (FileWriter file = new FileWriter("data.json")) {
+
+            file.write(json);
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
